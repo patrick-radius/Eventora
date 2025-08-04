090e54c28219b45b082878c32ef1fd5e84b598ff
patrick-radius <info@patrickradius.nl>
2025-08-04 22:37:16 +0200

    fix: add missing export

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { PostgresEventStore } from './PostgresEventStore.js'
 export { Command } from './types'
 export { Aggregate, CommandHandler, EventHandler, Projector, ProjectionHandler } from './decorators.js'
 export { commandDispatcher } from './bus';
+export { registerEvent } from './eventRegistry.js'
